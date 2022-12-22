a517330e38bda03d8d4bb6c7b4e7dec5dd1b7900
RasVV <v.rastrubin@bssys.com>
2022-12-22 14:19:14 +0600

    Initial Commit

---
+++ Closures/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Closures
+//
+//  Created by Karusel'ka . on 22.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
